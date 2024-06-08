fc3f3b3fe47f294eaf08c01ff9aeb2fdb14532aa
jhonestiago <jonestiago.94@gmail.com>
2024-06-08 13:04:16 -0300

    Mentoria

---
+++ bootcamp-oo/src/model/Mentoria.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.time.LocalDateTime;
+public class Mentoria {
+    
+    // Atributos
+    private String tema;
+    private LocalDateTime dataHora;
+
+    // Construtor
+    public Mentoria(String tema, LocalDateTime dataHora) {
+        this.tema = tema;
+        this.dataHora = dataHora;
+    }
+
+    // Getters e Setters
+    public String getTema() {
+        return this.tema;
+    }
+
+    public void setTema(String tema) {
+        if (tema != null && !tema.isEmpty()) {
+            this.tema = tema;
+        } else {
+            throw new IllegalArgumentException("Tema inválido");
+        }
+    }
+
+    public LocalDateTime getDataHora() {
+        return this.dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        if (dataHora != null) {
+            this.dataHora = dataHora;
+        } else {
+            throw new IllegalArgumentException("Data e hora inválido");
+        }
+    }
+
+}
